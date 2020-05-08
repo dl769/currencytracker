1e1316d21748dcda315c056f56b7ba7b7f32ed6d
dl769 <qwertyx7xx@gmail.com>
2020-05-09 00:02:04 +0200

    init

---
+++ script/script.js
@@ -0,0 +1,22 @@
+function getpage(pageurl){
+    $.get("https://images"+~~(Math.random()*33)+"-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=" + encodeURI(pageurl), function(data) {
+        console.log(data)
+    });
+}
+
+$(document).ready(function(){
+$('.z').on('click',function(){
+
+    let url = $('#inp').val();
+
+    getpage(url);
+
+
+})
+});
+
+/*
+https://www.walutomat.pl/kursy-walut/
+https://cinkciarz.pl/wymiana-walut/kursy-walut-cinkciarz-pl
+https://liderwalut.pl/kursy-walut
+
